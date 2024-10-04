000e92a523a525698426d8165dda74d6c1b53965
WelRaj <rajkumar.rathor.250925@gmail.com>
2024-10-05 00:35:24 +0530

    commit add

--- linkedlist/deletioninDLL.cpp
+++ linkedlist/deletioninDLL.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
 using namespace std;
 
 struct node {
@@ -104,4 +104,4 @@ int main() {
         temp = temp->next;
     }
     return 0;
-}
+}                    
