fbfc4ee144abe389f7d235978c0ce96ed96c011d
WelRaj <rajkumar.rathor.250925@gmail.com>
2025-01-08 20:15:02 +0530

    commit changes

--- dom project/new project/script.js
+++ dom project/new project/script.js
@@ -3,6 +3,28 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+var timeout;
+function circlesize(){
+     var xscale= 1;
+     var yscale= 1;
+     var xprev=0;
+     var yprev=0;
+     window.addEventListener("mousemove", function(dets){
+        this.clearTimeout(timeout); 
+        var xdiff=dets.clientX-xprev;
+         var ydiff=dets.clientY-yprev;
+         xscale=gsap.utils.clamp(.8,1.2,xdiff);
+         xscale=gsap.utils.clamp(.8,1.2,ydiff);      
+         xprev= dets.clientX;
+         yprev= dets.clientY;
+         circleMouseFollower(xscale,yscale);
+        timeout=setTimeout(function(){
+         document.querySelector("#minicircle").style.transform= `translate(${dets.clientX}px,${dets.clientY}px) scale(1,1)`; 
+            
+         },100);
+    });
+}
+circlesize();
 function firstPageAnim(){
     var tl = gsap.timeline();
     tl.from("#nav",{
@@ -26,10 +48,10 @@ function firstPageAnim(){
     ease: Expo.easeInOut
    })
 }
- function circleMouseFollower(){
+ function circleMouseFollower(xscale,yscale){
      window.addEventListener("mousemove",function(dets){
-     document.querySelector("#minicircle").style.transform= `translate(${dets.clientX}px,${dets.clientY}px)`; 
-     })
+     document.querySelector("#minicircle").style.transform= `translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale},${yscale})`; 
+     });
  }
  circleMouseFollower();
 
